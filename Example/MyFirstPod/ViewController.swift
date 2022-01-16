e808493d25475fe1c6de4d67713b8930588cd4b2
Greenskiii <alexdanevich@icloud.com>
2022-01-16 23:01:32 +0200

    version 0.2.0

--- Example/MyFirstPod/ViewController.swift
+++ Example/MyFirstPod/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import MyFirstPod
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    let url = "https://sun9-18.userapi.com/sun9-81/s/v1/ig2/D3r0uxLYycMoT6LIxDkxocJXIBq6ruCHidXAKxbDT36YdCDsHcXVmvj_H5C3kNUVRar31z2ExBJhIQ2kFixET8gl.jpg?size=200x200&quality=96&crop=10,0,950,950&ava=1"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imageView.setImage(from: url)
     }
 
     override func didReceiveMemoryWarning() {
--- Source/Pod.swift
+++ Source/Pod.swift
@@ -5,9 +5,20 @@
 //  Created by Алексей Даневич on 14.01.2022.
 //
 
-import Foundation
+import UIKit.UIImage
 
-
-class Pod {
+extension UIImageView {
     
+    public func setImage(from urlString: String) {
+        let url = URL(string: urlString)!
+        
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: url) {
+                DispatchQueue.main.async {
+                    self.image = UIImage(data: data)
+                }
+            }
+        }
+    }
 }
+
